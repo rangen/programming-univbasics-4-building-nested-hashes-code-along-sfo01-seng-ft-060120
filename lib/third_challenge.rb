a99bc923defd5a92b61c2b6d5b2aa98c30e37a68
Don Mallory <dmm333@gmail.com>
2020-02-20 08:41:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,7 +4,7 @@ def third_challenge
       :patriarch => {
           name: "Lord Montague",
           age: "53"
-          "},
+          },
       :matriarch => {
           name: "Lady Montague",
           age: "54"
